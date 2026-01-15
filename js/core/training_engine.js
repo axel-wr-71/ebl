fbf24f89a20d8bf79f87df922f1df2a3d4b53851
axel-wr-71 <strubbe23@gmail.com>
2026-01-15 17:38:08 +0100

    Create training_engine.js

---
+++ js/core/training_engine.js
@@ -0,0 +1,11 @@
+// js/core/training_engine.js
+
+export async function runAutomaticTraining(seasonId) {
+    // 1. Pobierz wszystkie zespoły i ich ustawione plany treningowe
+    // 2. Pobierz wszystkich zawodników
+    // 3. Dla każdego zawodnika przelicz rozwój:
+    //    Wzrost = (TypTreningu * Baza) + (Potencjał * ModyfikatorWieku) + (Lojalność)
+    
+    console.log("Symulacja treningu w toku...");
+    // Tutaj wejdzie silnik, który wymyślimy w następnym kroku
+}
